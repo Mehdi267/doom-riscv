70ddb3cf10a43f77c3df9528ae328a4eab57daf9
Mehdi <yaboi@gmail.com>
2023-08-06 23:52:10 +0200

    Fixed build error

--- src/user/tests/tlib/sysapi.h
+++ src/user/tests/tlib/sysapi.h
@@ -265,20 +265,20 @@ enum SEEK_OPERATION {
   SEEK_END = 2,
 };
 
-struct stat {
-    dev_t     st_dev;         // ID of device containing file
-    ino_t     st_ino;         // Inode number
-    mode_t    st_mode;        // File type and mode
-    nlink_t   st_nlink;       // Number of hard links
-    uid_t     st_uid;         // User ID of owner
-    gid_t     st_gid;         // Group ID of owner
-    dev_t     st_rdev;        // Device ID (if file is character or block special)
-    off_t     st_size;        // Total size, in bytes
-    blksize_t st_blksize;     // Block size for filesystem I/O
-    blkcnt_t  st_blocks;      // Number of 512B blocks allocated
-    time_t    st_atime;       // Time of last access
-    time_t    st_mtime;       // Time of last modification
-    time_t    st_ctime;       // Time of last status change
+struct __attribute__((packed)) stat {
+  dev_t     st_dev;         // ID of device containing file
+  ino_t     st_ino;         // Inode number
+  mode_t    st_mode;        // File type and mode
+  nlink_t   st_nlink;       // Number of hard links
+  uid_t     st_uid;         // User ID of owner
+  gid_t     st_gid;         // Group ID of owner
+  dev_t     st_rdev;        // Device ID (if file is character or block special)
+  off_t     st_size;        // Total size, in bytes
+  blksize_t st_blksize;     // Block size for filesystem I/O
+  blkcnt_t  st_blocks;      // Number of 512B blocks allocated
+  time_t    st_atime;       // Time of last access
+  time_t    st_mtime;       // Time of last modification
+  time_t    st_ctime;       // Time of last status change
 };
 
 //Fs api 
